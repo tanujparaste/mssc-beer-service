c3776d118e200f79b49e7352fb525feec5c16df7
Tanuj Paraste <tanuj.paraste27@gmail.com>
2023-05-12 20:49:49 +0530

    updated file

--- src/main/java/com/learningprojects/web/controller/BeerController.java
+++ src/main/java/com/learningprojects/web/controller/BeerController.java
@@ -3,7 +3,6 @@ package com.learningprojects.web.controller;
 import java.util.UUID;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
