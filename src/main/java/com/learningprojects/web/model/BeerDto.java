3e4925a07d1a5a8c730975e0759b2832e535de65
Tanuj Paraste <tanuj.paraste27@gmail.com>
2023-05-12 20:34:33 +0530

    added web data model. closes #2

---
+++ src/main/java/com/learningprojects/web/model/BeerDto.java
@@ -0,0 +1,33 @@
+package com.learningprojects.web.model;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BeerDto {
+	private UUID id;
+	private Integer version;
+
+	private OffsetDateTime createdDate;
+	// usually in web app, we typically use UTC type of date
+	private OffsetDateTime lastModifiedDate;
+
+	private String beerName;
+
+	private BeerStyleEnum beerStyle;
+
+	private Long upc;
+
+	private BigDecimal price;
+
+	private Integer quantityOnHand;
+}
