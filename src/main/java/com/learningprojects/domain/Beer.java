9c0ceb9f9e2708a20d6838c62c222dfe623ee163
Tanuj Paraste <tanuj.paraste27@gmail.com>
2023-05-13 07:28:57 +0530

    added jpa entity for beer object. closes #5

---
+++ src/main/java/com/learningprojects/domain/Beer.java
@@ -0,0 +1,55 @@
+package com.learningprojects.domain;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Version;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Beer {
+	@Id
+	@GeneratedValue(generator = "uuid")
+	@GenericGenerator(name = "uuid", strategy = "org.hibernate.id.uuid.UuidGenerator")
+	@Column(length = 36, columnDefinition = "varchar", updatable = false, nullable = false)
+	private UUID id;
+
+	@Version // gives optimistic locking
+	private Integer version;
+
+	@CreationTimestamp
+	@Column(updatable = false)
+	private Timestamp createdDate;
+
+	@UpdateTimestamp
+	private Timestamp lastModifiedDate;
+	
+	private String beerName;
+	private String beerStyle;
+
+	@Column(unique = true)
+	private Long upc;
+	
+	private BigDecimal price;
+
+	private Integer minOnHand;
+	private Integer quantityToBrew;
+}
