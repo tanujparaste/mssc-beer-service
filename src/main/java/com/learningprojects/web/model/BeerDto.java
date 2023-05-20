8d37deea71335086a3653ba25cba266dfdd4bfc0
Tanuj Paraste <tanuj.paraste27@gmail.com>
2023-05-20 09:52:42 +0530

    added jackson configuration for beerdto. closes #13

--- src/main/java/com/learningprojects/web/model/BeerDto.java
+++ src/main/java/com/learningprojects/web/model/BeerDto.java
@@ -4,6 +4,9 @@ import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.UUID;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonFormat.Shape;
+
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Null;
@@ -26,9 +29,11 @@ public class BeerDto {
 	private Integer version;
 
 	@Null
+	@JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ", shape = Shape.STRING)
 	private OffsetDateTime createdDate;
 	// usually in web app, we typically use UTC type of date
 	@Null
+	@JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ", shape = Shape.STRING)
 	private OffsetDateTime lastModifiedDate;
 
 	@NotBlank
@@ -43,6 +48,7 @@ public class BeerDto {
 
 	@Positive
 	@NotNull
+	@JsonFormat(shape = Shape.STRING)
 	private BigDecimal price;
 
 	private Integer quantityOnHand;
